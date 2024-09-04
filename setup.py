4d80cdb17828c74e482a9e216672a690d4da7e78
kolli-balaram <kollibalaram5@gmail.com>
2024-09-04 10:22:45 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="PROJECTS",
+    author="Balaram",
+    version="0.0.1",
+    author_email='kollibalaram5@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
+
